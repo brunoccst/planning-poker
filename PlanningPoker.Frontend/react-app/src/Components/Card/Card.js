bcaa29fc2babe9c3d3f72d185dd398f58be9ac60
Bruno Carvalho da Costa <brunoccst@gmail.com>
2022-05-04 22:51:43 +0200

    Styled cards

--- PlanningPoker.Frontend/react-app/src/Components/Card/Card.js
+++ PlanningPoker.Frontend/react-app/src/Components/Card/Card.js
@@ -1,8 +1,12 @@
 import './Card.scss';
 
+const ExpectedValues = [0, 1, 2, 3, 5, 8, 13, 20, "?", "∞", "☕"];
+
 const Card = ({ value }) => {
+    let valueClass = ExpectedValues.indexOf(value);
+
     return (
-        <div className="card">
+        <div className={`card value-${valueClass}`}>
             <div className="rounded-value upper-value">{value}</div>
             <div className="card-outer">
                 <div className="card-inner">
