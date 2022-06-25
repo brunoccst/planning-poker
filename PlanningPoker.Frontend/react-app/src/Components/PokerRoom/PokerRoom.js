244d043261b90d93002a17e48928502744a7247e
Alexseki <alexseki@gmail.com>
2022-06-25 17:33:59 -0300

    Created method to show the options when it starts.

--- PlanningPoker.Frontend/react-app/src/Components/PokerRoom/PokerRoom.js
+++ PlanningPoker.Frontend/react-app/src/Components/PokerRoom/PokerRoom.js
@@ -2,17 +2,24 @@ import './PokerRoom.scss';
 import Card from '../Card/Card';
 import User from '../User/User';
 import {useParams} from 'react-router-dom';
+import { useState } from 'react';
 
 const PokerRoom = () => {
     let { roomName } = useParams();
     let { userName } = useParams();
     let { isScrumMaster } = useParams();
+    const [ showOptions , setShowOption] = useState(false);
+    const onStartClicked = (evt) => {
+        //Show the options to be selected
+        setShowOption(true);
+    };
     let cards = [];
-    for (var i = 0; i < 10; i++) {
+    var i;
+    for (i = 0; i < 10; i++) {
         cards.push(<Card value={i}/>);
     }
     let users = [];
-    for (var i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++) {
         users.push(<User userName={"John"+i}/>);
     }
     return (
@@ -20,6 +27,8 @@ const PokerRoom = () => {
             <div>
                 <h1>Poker Room - {roomName}</h1>
                 <h2>{userName} - {isScrumMaster}</h2>
+                <button type='button' onClick={onStartClicked}>Start</button>
+                <h3>{showOptions}</h3>
             </div>
             
             <h2>Result</h2>
@@ -27,12 +36,15 @@ const PokerRoom = () => {
                 {/* Show the users and their cards */}
                 {users}
             </div>
-
-            <h2>Choose</h2>
-            <div className='pokerRoomCardSet'>
-                {/* The user can select his cards */}
-                {cards}
-            </div>
+            { showOptions && (
+                <div name='divChoose'>
+                    <h2>Choose</h2>
+                    <div className='pokerRoomCardSet'>
+                        {/* The user can select his cards */}
+                        {cards}
+                    </div>
+                </div>
+            )}
 
         </div>
     );
